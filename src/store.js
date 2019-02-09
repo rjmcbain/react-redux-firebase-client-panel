5631f51b5696fddbe1f51fa59eb1d29abea412a5
rjmcbain <rmcbain01@gmail.com>
2019-02-08 17:06:15 -0700

    adding auth

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ import "firebase/firestore";
 import { reactReduxFirebase, firebaseReducer } from "react-redux-firebase";
 import { reduxFirestore, firestoreReducer } from "redux-firestore";
 //Reducers
-// todo
+import notifyReducer from "./reducers/notifyReducer";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCTEDXPW1-wi6aQJJAlz8skxq3u_RbeO9Q",
@@ -39,7 +39,8 @@ const createStoreWithFirebase = compose(
 // Add firebase to reducers
 const rootReducer = combineReducers({
   firebase: firebaseReducer,
-  firestore: firestoreReducer // <- needed if using firestore
+  firestore: firestoreReducer, // <- needed if using firestore
+  notify: notifyReducer
 });
 
 // Create initial state
